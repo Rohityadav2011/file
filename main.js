91cf1c896b890e3efe68aceb45ed2a1c0c03e400
Rohityadav2011 <75617805+Rohityadav2011@users.noreply.github.com>
2020-12-07 17:23:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+
+ctx.beginpath();
+ctx.strokestyle="grey";
+ctx.linewidth=4;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="blue";
+ctx.linewidth=7
+ctx.arc(250, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="black";
+ctx.linewidth=7
+ctx.arc(350, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="red";
+ctx.linewidth=7
+ctx.arc(350, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="yellow";
+ctx.linewidth=7
+ctx.arc(350, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="green";
+ctx.linewidth=7
+ctx.arc(350, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
